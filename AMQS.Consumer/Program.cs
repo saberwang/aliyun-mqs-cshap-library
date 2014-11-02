74e3f9e7c4619cd1e90edcfa128c3bbb49008a4c
Chris <fzhenmei@gmail.com>
2014-11-02 16:36:00 +0800

    添加主程序文件

---
+++ AMQS.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AMQS.Consumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit.");
+
+            Task.Factory.StartNew(ReceiveMessage);
+            Task.Factory.StartNew(ReceiveMessage);
+            Task.Factory.StartNew(ReceiveMessage);
+
+            Console.ReadKey();
+        }
+
+        private static void ReceiveMessage()
+        {
+            var mqClient =
+                new MQSClient(string.Format("http://{0}.mqs-cn-hangzhou.aliyuncs.com", AliConfig.queueownerId),
+                    AliConfig.accessKeyId, AliConfig.accessKeySecret);
+
+            while (true)
+            {
+                MQSQueue queue = mqClient.getQueue("testmq1");
+                MessageReceiveResponse message = queue.popMessage();
+
+                if (!string.IsNullOrWhiteSpace(message.Code)) //有错误
+                {
+                    Console.WriteLine(message.Message);
+                    continue;
+                }
+
+                Console.WriteLine(message.MessageBody);
+                queue.deleteMessage(message.ReceiptHandle);
+            }
+        }
+    }
+}
---
+++ AMQS.Productor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AMQS.Consumer;
+
+namespace AMQS.Productor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit.");
+
+            Task.Factory.StartNew(() => SendMessage());
+            Task.Factory.StartNew(() => SendMessage());
+            Task.Factory.StartNew(() => SendMessage());
+
+            Console.ReadKey();
+        }
+
+        private static void SendMessage()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var mqClient =
+                    new MQSClient(string.Format("http://{0}.mqs-cn-hangzhou.aliyuncs.com", AliConfig.queueownerId),
+                        AliConfig.accessKeyId, AliConfig.accessKeySecret);
+
+                MQSQueue queue = mqClient.getQueue("testmq1");
+                string message = string.Format("Hello World！ <from {0}, No.{1}>", Thread.CurrentThread.ManagedThreadId,
+                    i);
+                queue.sendMessage(message);
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
